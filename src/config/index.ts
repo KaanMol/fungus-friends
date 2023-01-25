2de8295d884649776d135e68aabda2c8abb90ff4
Kaan <kaanmol@outlook.com>
2023-01-26 00:32:35 +0100

    Added custom tile provider

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@
 const config = {
     map: {
         center: [52.08112405855099, 5.235028521757173],
-        provider: "",
+        provider: "https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token=9D54c4f49OZE8CvAgkNiDPoeb2cDPFWzzBFYWu0WbU1uYqLEszjZIjtpzXPGJoR5",
         zoom: 17
     }
 }
